f8e6389638131379ce04ebc31951f0b5f180e77f
tech-digs <lkang9194@gmail.com>
2025-07-11 19:30:03 +0100

    deliverables message in backend/views.py

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# deliverables::
+        #create packages.
+        #create code generating code
+        #create the m-pesa bridge
+        #create the mikrotic-with python connection.
